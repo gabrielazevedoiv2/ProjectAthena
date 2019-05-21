027694ff360a0bc51e53343905e35db67109fe3a
gabriel.azevedo@iv2.com.br <gabriel.azevedo@iv2.com.br>
2019-05-21 08:38:09 -0300

    ok

--- src/RPSGame/gameController.js
+++ src/RPSGame/gameController.js
@@ -1,7 +1,28 @@
 var socket;
-window.onload = (e) => {
+let playerId;
+window.onload = (window) => {
     socket = io.connect('http://localhost:3000');
     socket.emit('playerConnected', {"game": "rps"});
+    socket.on('matchStarted', (e) => {
+        console.log(e)
+        playerId = e.playerId;
+    });
+    document.querySelectorAll('#rock, #scissors, #paper').forEach((x) => {
+        x.onclick = (e) => {
+            document.querySelector('#mychoice').innerHTML = 'My Choice: ' + e.currentTarget.value;
+            socket.emit('playerChoice', {value: e.currentTarget.value});
+        }
+    })
+    socket.on('matchResult', (e) => {
+        if (e.result.winner.id == playerId) {            
+            document.querySelector('#theirchoice').innerHTML = 'Their choice: ' + e.result.loser.choice;
+            alert('you win!');
+        } else {
+            document.querySelector('#theirchoice').innerHTML = 'Their choice: ' + e.result.winner.choice;
+            alert('you lose :(');
+        }        
+        console.log(e);
+    })
 }
 
 class GameController {
--- src/RPSGame/rpsServerController.js
+++ src/RPSGame/rpsServerController.js
@@ -1,6 +1,13 @@
+// ====================================================================
+// RPS Server Controller by Gabriel Azevedo
+// ====================================================================
+// To-do:
+// 1. Implement socket.io rooms logic
+// 2. Clean-up code
+
 module.exports = class RPSServerController {
-    constructor(io) {
-        this.io = io;
+    constructor(socket) {
+        this.socket = socket;
         this.players = [];
         this.currentMatches = [];
     }
@@ -15,6 +22,10 @@ module.exports = class RPSServerController {
             console.log(client.id + ' disconnected');
             this.popPlayer(client.id);
         });
+        const unmatchedPlayers = this.players.filter((x) => x.inMatch == false);
+        if (unmatchedPlayers.length > 1) {
+            this.startMatch(unmatchedPlayers[0], unmatchedPlayers[1]);
+        }
     }
 
     popPlayer(id) {
@@ -22,8 +33,12 @@ module.exports = class RPSServerController {
     }
 
     startMatch(player1, player2) {
+        this.players.find((x) => x.id == player1.id).inMatch = true;
+        this.players.find((x) => x.id == player2.id).inMatch = true;
         // Develop match logic
+        const matchId = 'match_'+new Date().toLocaleString();
         this.currentMatches.push({
+            matchId: matchId,
             player1: {
                 id: player1.id,
                 choice: ''
@@ -33,5 +48,63 @@ module.exports = class RPSServerController {
                 choice: ''
             }
         });
+        this.socket.to(player1.id).emit('matchStarted', {
+            playerId: player1.id,
+            matchId: matchId
+        });
+        this.socket.to(player2.id).emit('matchStarted', {
+            playerId: player2.id,
+            matchId: matchId
+        });
+        player1.client.once('playerChoice', (e) => {
+            const match = this.currentMatches.find(x => x.matchId == matchId);
+            match.player1.choice = e.value;
+            if (match.player1.choice != '' && match.player2.choice != '') {
+                const result = this.getMatchResult(match);
+                if (result != 'draw') {
+                    this.socket.to(player1.id).emit('matchResult', {
+                        result: result
+                    });
+                    this.socket.to(player2.id).emit('matchResult', {
+                        result: result
+                    });
+                }
+            }
+        });        
+        player2.client.once('playerChoice', (e) => {
+            const match = this.currentMatches.find(x => x.matchId == matchId);
+            match.player2.choice = e.value;
+            if (match.player1.choice != '' && match.player2.choice != '') {
+                const result = this.getMatchResult(match);
+                if (result != 'draw') {
+                    this.socket.to(player1.id).emit('matchResult', {
+                        result: result
+                    });
+                    this.socket.to(player2.id).emit('matchResult', {
+                        result: result
+                    });
+                }
+            }
+        });
+    }
+
+    getMatchResult(match) {
+        if ((match.player1.choice == 'rock' && match.player2.choice == 'scissors') || 
+            (match.player1.choice == 'scissors' && match.player2.choice == 'paper') ||
+            (match.player1.choice == 'paper' && match.player2.choice == 'rock')) {
+            return {
+                winner: match.player1,
+                loser: match.player2,
+            }
+        } else if ((match.player2.choice == 'rock' && match.player1.choice == 'scissors') || 
+            (match.player2.choice == 'scissors' && match.player1.choice == 'paper') ||
+            (match.player2.choice == 'paper' && match.player1.choice == 'rock')) {
+            return {
+                winner: match.player2,
+                loser: match.player1
+            }   
+        } else {
+            return 'draw';
+        }
     }
 }
